658d78775802ca79adf88e49de0f4bba47ee0772
t2-g <89742029+t2-g@users.noreply.github.com>
2022-03-24 23:07:29 +0530

    commit 2

---
+++ tic-tac-toe-minimax.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void board()
+{
+	system("cls");
+	cout << "\n\n\tTic Tac Toe\n\n";
+	//cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+	cout << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+	cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+	cout << "     |     |     " << endl << endl;
+}
+
+int main()
+{
+    char cont ='y';
+    int player = 1,i,choice;
+	char mark;
+    do
+    {
+        
+    }
+}
+
